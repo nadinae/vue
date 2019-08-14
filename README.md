3f5a873494eecc7f1ba554593efb522e38981c62
nadinae <guangli@cecelive.com>
2019-08-14 19:43:41 +0800

    vue

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # vue
 vue源码解析及一些基本功能实现原理
+
+# vue底层原理关系图
+
+![](img/vue.png)
+
+vue是通过Object.defineProperty()方法对数据劫持来实现双向绑定的。
+
+其中通过Observer劫持监听所有属性，当数据有变化时，通知Dep。指令解析器Compile对每个元素的指令进行扫描和解析，然后对指令的模板进行数据替换，并绑定Update函数。
+
+
---
+++ myVue.js
@@ -0,0 +1,71 @@
+class MyVue{
+  constructor(options){
+    this.$data = options.data;
+    this.$options = options;
+
+    this.observe(this.$data)
+
+    new Watcher()
+    this.$data.name
+  }
+
+  //设置监听函数
+  observe(data) {
+    if(!data || typeof data !== 'object'){
+      return;
+    }
+    Object.keys(data).forEach((value) => {
+
+      this.observe(value)//递归加查数据是否有多层嵌套
+
+      this.defineReactive(data,value,data[value])
+    })
+  }
+
+  //设置数据响应化
+  defineReactive(data,key,value){
+    const dep = new Dep()
+    Object.defineProperty(data,key,{
+      configurable:true,
+      enumerable:true,
+      set(newValue){
+        if (value == newValue) return;
+        value = newValue
+        dep.notify();
+      },
+      get() {
+        Dep.target && dep.addDeps(Dep.target);
+        return value;
+      }
+    })
+  }
+}
+
+//设置Dep,用来管理watcher
+class Dep{
+  constructor(){
+    this.deps = []//设置deps用来管理依赖
+  }
+
+  //用来收集依赖
+  addDeps(dep){
+    this.deps.push(dep)
+  }
+
+  //设置通知函数
+  notify(){
+    this.deps.forEach(dep => dep.update())
+  }
+}
+
+//设置watcher
+class Watcher{
+  constructor() {
+    // 将当前watcher实例指定到Dep静态属性target
+    console.log(this)
+    Dep.target = this;
+  }
+  update() {
+    console.log("属性更新了");
+  }
+}
